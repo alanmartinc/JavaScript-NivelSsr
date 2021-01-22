20b0014268daee1e6523b88a6ba1031483e3ad67
alanmartinc <alancabot.ac@gmail.com>
2021-01-22 17:30:55 -0300

    Se elimina ejercicios del capitulo 8

--- capitulo8.js
+++
@@ -1,8 +0,0 @@
-document.querySelector("body > div > p ").style.color = "grey";
-
-/*
-Herramienta Desarrollador:
-*/
-
-// Elements: CTRL + SHIFF + P: Seleccionar Atajos (Ejemplo: Console).
-// Sources - Snippets: Crear Script, con Ctrl + Enter se ejecuta.
