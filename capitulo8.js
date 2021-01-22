0e4ae2f8f940a0d2b6149da555584ffcd7a39c3b
alanmartinc <alancabot.ac@gmail.com>
2021-01-22 17:31:14 -0300

    Se agrega ejercicios del capitulo 8

---
+++ capitulo8.js
@@ -0,0 +1,8 @@
+document.querySelector("body > div > p ").style.color = "grey";
+
+/*
+Herramienta Desarrollador:
+*/
+
+// Elements: CTRL + SHIFF + P: Seleccionar Atajos (Ejemplo: Console).
+// Sources - Snippets: Crear Script, con Ctrl + Enter se ejecuta.
